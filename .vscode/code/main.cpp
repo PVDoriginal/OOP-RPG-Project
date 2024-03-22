162c7f6c28fddff0f676f34a4710f7e53d0f5e3c
Damon <pvdoriginal2019@gmail.com>
2024-03-22 20:36:12 +0200

    header/cpp separation

---
+++ .vscode/code/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include "Headers/character.h"
+#include "Headers/items.h"
+#include "Headers/shop.h"
+
+    void InitiateShop(Shop& shop){
+        shop.AddItem(new Item("small potion", 5, SmallHeal{}));
+        shop.AddItem(new Item("small potion", 5, SmallHeal{}));
+        shop.AddItem(new Item("medium potion", 10, MediumHeal{}));
+        shop.AddItem(new Item("big potion", 15, BigHeal{}));
+    }
+
+    int main(){
+        Character Damon;
+        Shop shop;
+
+        InitiateShop(shop);
+        shop.CheckItems();
+        shop.BuyItem("small potion", Damon);
+        Damon.CheckInventory();
+        shop.CheckItems();
+    }
